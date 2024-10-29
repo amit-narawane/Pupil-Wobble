71e870aa0eb25d5e5dd193e62d74a453cd2d1975
amit-narawane <135150216+amit-narawane@users.noreply.github.com>
2024-10-29 15:00:15 -0400

    Added extra comments

--- ZMXconfig.py
+++ ZMXconfig.py
@@ -9,14 +9,14 @@ class BaseConfig():
         ### Zemax Parameters ###
         ########################
 
-        # Lens surfaces for scanning mirrors
+        # Lens surfaces for the tilt (coordinate break) for scanning mirrors
         self.x_galvo_sur = 20
         self.y_galvo_sur = 25
-        self.fsm_sur = 40
+        self.fsm_sur = 40               # Will be ignored if no FSM present
         self.pupil_plane = 60
         self.pupil = True               # True for tracing at pupil plane, False for tracing at retina/image plane
 
-        # Scan parameters
+        # Scan parameters (scan dimensions in mechanical angle)
         self.scan_type = 'radial'
         self.scan_dim = 5
         self.scan_range = [-self.scan_dim,self.scan_dim]
@@ -29,9 +29,9 @@ class BaseConfig():
         self.fsm_slope = 1              # mm/deg calibration
 
         # Raytrace parameters
-        self.waveNumber = 2
-        self.hx = 0
-        self.hy = 0
+        self.waveNumber = 2             # Choose wavelength
+        self.hx = 0                     # Leave normalized field coordinate 0
+        self.hy = 0                     # Leave normalized field coordinate 0
         self.n_ring = 1                 # 1 for chief ray, n to trace n marginal rays (ring)
 
 
